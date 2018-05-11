a49923269bed41bebdab893f2286759255c30003
jchhina2 <35819986+jchhina2@users.noreply.github.com>
2018-05-10 23:13:28 -0500

    Update Olympic-Countdown.js

--- Olympic-Countdown.js
+++ Olympic-Countdown.js
@@ -1,11 +1,13 @@
 
 <script>
 function myFunction() {
-    var d = new Date();
+    
+    //User's current Date & Time
+    var currentDate = new Date();
     
     //Opening Ceremony date
     var openingDate = new Date("July 24, 2020");
-    var n = d.toLocaleTimeString();
+    var n = currentDate.toLocaleTimeString();
     document.getElementById("demo").innerHTML = n;
 }
 </script>
