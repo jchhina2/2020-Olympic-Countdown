2f55d66f7b517694b8e690dec48ecc838d4a24f8
Jasraj Chhina <35819986+jchhina2@users.noreply.github.com>
2018-06-17 20:47:54 -0700

    Update Olympic-Countdown.js

--- Olympic-Countdown.js
+++ Olympic-Countdown.js
@@ -1,12 +1,16 @@
 
 function countDown() {
     
-    //User's current Date & Time
+    //Get the User's current Date & Time
     var currentDate = new Date().getTime();
     
-    //Opening Ceremony date
-    var openingDate = new Date("July 24, 2020");
-    var n = currentDate.toLocaleTimeString();
-    document.getElementById("demo").innerHTML = n;
+    //Create the Opening Ceremony date
+    var openingDate = new Date("July 24, 2020").getTime();
+   
+    //Updates the countdown every 1000 milliseconds, or 1 second
+    var beginCountdown = setInterval(function(){
+        var n = currentDate.toLocaleTimeString();
+        document.getElementById("demo").innerHTML = n;
+    },1000);        
 }
 
