f2a3325b79782073988ecf9720d1feb867f78dec
Jason Chhina <35819986+jchhina2@users.noreply.github.com>
2019-02-23 20:31:16 -0600

    Update Olympic-Countdown.js

--- Olympic-Countdown.js
+++ Olympic-Countdown.js
@@ -5,7 +5,7 @@ function countDown() {
     var currentDate = new Date().getTime();
     
     //Create the Opening Ceremony date
-    var openingDate = new Date("July 24, 2020").getTime();
+    var openingDate = new Date("July 24, 2020 00:00:00").getTime();
    
     //Updates the countdown every 1000 milliseconds, or 1 second
     var beginCountdown = setInterval(function(){
@@ -16,7 +16,7 @@ function countDown() {
         
         //The math that will calculate the difference
         var daysBeforeOpening = Math.floor(timeBeforeOpening / (1000 * 60 * 60 * 24));
-        const demo = document.createElement("demo");
+        //const demo = document.createElement("demo");
         document.getElementById("demo").innerHTML = daysBeforeOpening;
     },1000);        
 }
